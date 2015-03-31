83f1c365943cded6d26e0c1ad381efe20cdbc481
felix <felixwebkit@gmail.com>
2015-03-31 16:06:10 +0800

    init;

--- app.js
+++ app.js
@@ -1,21 +1,80 @@
 // Initialize Swiper
 $(document).ready(function () {
-    //initialize swiper when document ready
-    var mySwiper = new Swiper ('.swiper-container', {
-        // Optional parameters
-        direction: 'vertical',
-        onInit: function(swiper){
-            hideSlide();
-            showOnebyOne();
-        },
-        onTransitionEnd: function(swiper){
-            hideSlide();
-            showOnebyOne();
-        },
-        onTransitionStart: function(swiper){
-            console.log(swiper)
-        }
-    });
+
+
+    var isOK = false;
+    var preload;
+
+    function init() {
+        // Create a new queue.
+        preload = new createjs.LoadQueue(false, "imgs/");
+
+        var plugin = {
+            getPreloadHandlers: function () {
+                return {
+                    extensions: ["svg","mp3","png"],
+                    callback: function (item) {
+                        var id = item.src.toLowerCase().split("/").pop().split(".")[0];
+                        $("#"+id).attr("src", item.src);
+                    }
+                };
+            }
+        };
+
+        preload.installPlugin(plugin);
+        preload.loadManifest(["logo.svg"
+
+        ]);
+        preload.on("complete", handleComplete);
+
+    }
+
+    function handleComplete(event) {
+        overLoading();
+    }
+    init();
+
+
+    function overLoading(){
+        $('#loading').addClass("animated bounceOut").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+            $("#loading").removeClass("animated bounceOut");
+            hideOpacity($("#loading"));
+            $("#logo").css('opacity', '1');
+            $("#wave").css('opacity', '1');
+            $("#main-content").css('opacity', '1');
+            initSwiper();
+        });
+    }
+
+    hideSlide();
+    $("#logo").css('opacity', '0');
+    $("#wave").css('opacity', '0');
+    $('#main-content').css('opacity', '0');
+
+    var currentIndex = 0;
+    function initSwiper(){
+        //initialize swiper when document ready
+        var mySwiper = new Swiper ('.swiper-container', {
+            // Optional parameters
+            direction: 'vertical',
+            onInit: function(swiper){
+                hideSlide();
+                showOnebyOne();
+            },
+            onTransitionEnd: function(swiper){
+                if(currentIndex != swiper.activeIndex){
+                    showOnebyOne();
+                }
+                currentIndex = swiper.activeIndex;
+            },
+            onTransitionStart: function(swiper){
+                if(currentIndex != swiper.activeIndex){
+                    hideSlide();
+                }
+            }
+        });
+    }
+
 
     function hideOpacity(obj){
         obj.css('opacity', '0');
@@ -29,6 +88,7 @@ $(document).ready(function () {
         $('.swiper-slide').children().each(function(element){
             if($(this).attr('animated-css')){
                 hideOpacity($(this));
+                $(this).removeClass('animated ' + $(this).attr('animated-css'));
             }
         });
     }
